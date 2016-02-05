3349c328df0e85b3b3a97c0dcd56992c8e1f4d28
Manuel Riezebosch <mriezebosch@gmail.com>
2016-02-05 13:18:03 +0100

    lege solution, mvc met tests

---
+++ secdev-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="secdev_mvc.MvcApplication" Language="C#" %>
---
+++ secdev-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(secdev_mvc.Startup))]
+namespace secdev_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
